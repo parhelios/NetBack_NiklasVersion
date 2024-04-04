42a7d5c843a64b88affaa0a8e624b2696832f4dd
niklas-hjelm <niklas.hjelm@iths.se>
2024-04-04 10:21:28 +0200

    Add project files.

---
+++ NetBack.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetBack.UI.Data;
+
+namespace NetBack.Infrastructure;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+	{
+		var connectionString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+		services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseSqlServer(connectionString));
+
+		return services;
+	}
+}
